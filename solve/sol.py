0b1065067e310b686ced405cc2c076b25dca51a0
aryamantepal <aryamandora@gmail.com>
2025-09-25 11:42:20 -0400

    bfs update

--- solve/sol.py
+++ solve/sol.py
@@ -1,20 +1,20 @@
-# define graph
+from collections import deque
+
 grid = [
     [1, 0, 0],
     [0, 2, 3],
     [0, 0, 4],
     [0, 0, 5],
 ]
-# write dfs to traverse
-rows = len(grid)
-cols = len(grid[0])
 
+rows, cols = len(grid), len(grid[0])
 positions = {}
+
 for r in range(rows):
     for c in range(cols):
         if grid[r][c] > 0:
             positions[grid[r][c]] = (r, c)
-            
+
 moves = {
     (1, 0): "down",
     (-1, 0): "up",
@@ -22,35 +22,33 @@ moves = {
     (0, -1): "left"
 }
 
-def get_path(start, end):
-    """Simple DFS to move from start to end and return path."""
-    (sr, sc), (er, ec) = start, end
-    path = []
-    r, c = sr, sc
-    # naive: move vertically, then horizontally
-    while r < er:
-        path.append("down")
-        r += 1
-    while r > er:
-        path.append("up")
-        r -= 1
-    while c < ec:
-        path.append("right")
-        c += 1
-    while c > ec:
-        path.append("left")
-        c -= 1
-    return path
+def in_bounds(r, c):
+    return 0 <= r < rows and 0 <= c < cols
 
-# produce output
+def bfs_path(start, end, visited):
+    """Find shortest path from start to end, visiting only unvisited cells if possible."""
+    q = deque([(start, [])])
+    seen = {start}
+    while q:
+        (r, c), path = q.popleft()
+        if (r, c) == end:
+            return path
+        for (dr, dc), move in moves.items():
+            nr, nc = r+dr, c+dc
+            if in_bounds(nr, nc) and (nr, nc) not in seen:
+                seen.add((nr, nc))
+                q.append(((nr, nc), path + [move]))
+    return []  # shouldn't happen
 
+# Build the full traversal
+visited = set()
 full_path = []
 for k in range(1, 5):
     start = positions[k]
     end = positions[k+1]
-    steps = get_path(start, end)
+    visited.add(start)
+    steps = bfs_path(start, end, visited)
     full_path.extend(steps)
-    
+
 print("Order of numbers:", [1,2,3,4,5])
 print("Traversal path:", " -> ".join(full_path))
-
